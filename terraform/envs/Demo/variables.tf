04a0f67ee4a358f138ba7472f297d42f570796e9
samar-chamkhi <samar.chamkhi@lyvoc.com>
2024-07-04 14:26:45 +0200

    organizing files

--- terraform/envs/Demo/variables.tf
+++
@@ -1,6 +0,0 @@
-
-variable "form_id"{
-  type = string
-  description = "The progressive profiling form ID" 
-  default = "ap_4vJKwewDeSKSorf5t88UmU"
-}
--- terraform/modules/branding/branding.tf
+++ terraform/modules/branding/branding.tf
@@ -1,19 +1,7 @@
-# resource "auth0_custom_domain" "tenant_domain" {
-#   domain = "auth.example.com"
-#   type   = "auth0_managed_certs"
-# }
-
 resource "auth0_branding" "brand" {
-  # logo_url = "http://localhost:5173/logo.png"
-
   colors {
     primary         = "#0059d6"
     page_background = "#000000"
   }
-
-  universal_login {
-    # Ensure that "{%- auth0:head -%}" and "{%- auth0:widget -%}"
-    # are present in the body.
-    body = file("universal_login_body.html")
-  }
 }
+
